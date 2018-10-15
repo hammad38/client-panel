34acdf928723598a5e8b049c16f50e50560b0d4f
Sayyed Hammad Ali <hammad_ali447@yahoo.com>
2018-10-15 14:54:38 +0200

    load data from firebasestore and spinner added

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -1,34 +1,15 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
+import PropTypes from 'prop-types';
+import {compose} from 'redux';
+import {connect} from 'react-redux';
+import {firestoreConnect} from 'react-redux-firebase';
+
+import Spinner from '../layout/Spinner';
 
 class Clients extends Component {
   render() {
-    const clients = [
-      {
-        id: '1',
-        firstName: 'person',
-        lastName: '1',
-        email: 'person1@gmail.com',
-        phone: '111-111-111',
-        balance: '100'
-      },
-      {
-        id: '2',
-        firstName: 'person',
-        lastName: '2',
-        email: 'person2@gmail.com',
-        phone: '222-222-222',
-        balance: '200'
-      },
-      {
-        id: '3',
-        firstName: 'person',
-        lastName: '3',
-        email: 'person3@gmail.com',
-        phone: '333-333-333',
-        balance: '300'
-      }
-    ];
+    const {clients} = this.props;
 
     if(clients) {
       return(
@@ -69,10 +50,20 @@ class Clients extends Component {
         </div>
       );
     } else {
-      return <h1>loading...</h1>
+      return (<Spinner/>);
     }
 
   }
 }
 
-export default Clients;
+Clients.propTypes = {
+  firestore: PropTypes.object.isRequired,
+  clients: PropTypes.array
+}
+
+export default compose(
+  firestoreConnect([{collection: 'clients'}]),
+  connect((state, props) => ({
+    clients: state.firestore.ordered.clients
+  }))
+)(Clients);
