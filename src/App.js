a107ad633103330790ff4b4df9fa64c8112032fe
Sayyed Hammad Ali <hammad_ali447@yahoo.com>
2018-10-15 17:09:07 +0200

    edit client working

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import store from './store';
 import Navbar from './components/layout/Navbar';
 import Dashboard from './components/layout/Dashboard';
 import AddClient from './components/clients/AddClient';
+import EditClient from './components/clients/EditClient';
 import DetailClient from "./components/clients/DetailClient";
 
 
@@ -22,6 +23,7 @@ class App extends Component {
               <Switch>
                 <Route exact path="/" component={Dashboard}/>
                 <Route exact path="/client/add" component={AddClient}/>
+                <Route exact path="/client/edit/:id" component={EditClient}/>
                 <Route exact path="/client/:id" component={DetailClient}/>
               </Switch>
             </div>
